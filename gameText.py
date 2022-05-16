570e3dc8a2df17bf3f2558cfff2b4057ec9dc1be
Maximiliano E. Lutz <maximiliano.lutz1997@gmail.com>
2022-05-16 12:19:29 -0300

    Update gameText.py

--- gameText.py
+++ gameText.py
@@ -1,13 +1,58 @@
 aboutText = """
-  ----------------------------------------------------------------------
+--------------------------------------------------
 
-    Text detailing what this Hangman game is about.
+\033[4mAbout\033[0m
 
-  ----------------------------------------------------------------------\n"""
+Hangman is a simple word guessing game you can play in your computer instead 
+of using your notebooks or the chalkboard.
+
+This game is meant to be played with 2 persons or more.
+
+Made with Python, this game counts with a bunch of different ASCII pictures to 
+make it easier and more fun to follow your progression while guessing the word 
+one of the players wrote.
+
+Although the basic logic behind the game is complete and doesn't present any 
+flaws when running, my intention is to further develop this game either by 
+adding new elements, making it more visually appealing, or even by compressing 
+the code into a more efficient, advanced and cleaner way.
+
+Thank you for playing and most importantly,
+\033[4mEnjoy!\033[0m
+
+©2022 by iMakishi. (Maximiliano E. Lutz)
+
+--------------------------------------------------
+"""
 
 helpText = """
-  ----------------------------------------------------------------------
+--------------------------------------------------
 
-    Text providing help tips about the Hangman game.
+\033[4mHelp\033[0m
 
-  ----------------------------------------------------------------------\n"""
+Hangman is pretty straightforward game and easy to understand if you've never 
+heard about it.
+
+One of the players needs to first write the word to be guessed.
+
+  \033[4mImportant\033[0m
+
+  - BLANK inputs are not allowed.
+  - NUMERICAL/DIGIT inputs are not allowed.
+  - OFFENSIVE language inputs are not allowed and will not be accepted.
+
+Afterwards, the remaining player/es, will need to guess the word that was 
+written.
+
+Note that you won't be able to check what the word is by scrolling back to see 
+it, since it's been automatically erased in order to prevent this.
+
+There's only 7 mistakes allowed before it's \033[4mGAME OVER\033[0m.
+Be careful and sure about the guesses you type.
+
+Otherwise,
+
+\033[4mGood Luck & Enjoy!\033[0m
+
+--------------------------------------------------
+"""
